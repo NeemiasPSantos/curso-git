23d054b262d8d0342aa774042cfa2be1a3266e5b
Neemias Pereira <neemias.pereira3@gmail.com>
2022-01-29 16:55:33 -0300

    Mensagem como você está

--- AppTeste/Program.cs
+++ AppTeste/Program.cs
@@ -7,6 +7,7 @@ namespace AppTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Como você está?");
         }
     }
 }
