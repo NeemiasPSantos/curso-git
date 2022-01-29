2909db19b0ccaec32c150dea21573c03bb262224
Neemias Pereira <neemias.pereira3@gmail.com>
2022-01-29 16:50:51 -0300

    Projeto iniciado mostrando a mensagem Hello World

---
+++ AppTeste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
